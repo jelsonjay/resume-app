44b054fb7a065cd5531e79107cdcfd7ea8ac1901
Jelson J <jelson20outlook.com>
2020-10-14 10:56:58 +0100

    new resume using gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,44 @@
-import React from "react"
+import { data } from "./data"
+import "./App.css"
+import React, { Component } from "react"
+import ProductList from "./ProductList"
 
-export default function Home() {
-  return <div>Hello world!</div>
+class App extends Component {
+  state = {
+    products: data,
+    //productcopy: [],
+    productcopy: data,
+    btn: ["All", "React", "Javascript", "Vue", "Gatsby"],
+  }
+
+  // Filtering button goes here
+  handleBtn = event => {
+    console.log(event.target.value)
+    let productcopy
+    if (event.target.value === "All") {
+      productcopy = this.state.products
+    } else {
+      productcopy = this.state.products.filter(
+        item => item.title === event.target.value
+      )
+    }
+
+    this.setState({
+      productcopy: productcopy,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ProductList
+          products={this.state.productcopy}
+          handleBtn={this.handleBtn}
+          btn={this.state.btn}
+        />
+      </div>
+    )
+  }
 }
+
+export default App
